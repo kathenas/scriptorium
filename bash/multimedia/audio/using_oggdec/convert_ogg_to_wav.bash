e55de5926b9dbb53fc85ec5f330c0ffca4e37692
Phil Wyett <philip.wyett@kathenas.org>
2024-04-07 05:10:44 +0100

    Add script:
    * convert_ogg_to_wav.bash

---
+++ bash/multimedia/audio/using_oggdec/convert_ogg_to_wav.bash
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Author: Phil Wyett (kathenas) - philip.wyett@kathenas.org
+#
+# Website: https://kathenas.org
+#
+# Wiki: https://wiki.kathenas.org
+#
+# Buy me a coffee: https://www.buymeacoffee.com/kathenasorg
+#
+# License: GPLv3 - See 'LICENSE' in root directory of git repository or
+#          the license at https://www.gnu.org/licenses/gpl-3.0.en.html
+#
+# Copyright is asserted by the author.
+
+# Define some colours for output text.
+red_text='\033[0;31m'
+yellow_text='\033[0;33m'
+green_text='\033[32m'
+end_colour_text='\033[0m'
+
+# If script run with 'sh', inform to execute directly or invoke with 'bash'.
+if [ ! "$BASH_VERSION" ] ; then
+    printf "\n%b=== WARNING ===%b\n" "${yellow_text}" "${end_colour_text}"
+    printf "\nPlease do not use %bsh%b to run this script %b%s%b. Run directly \
+e.g. %b./%s%b if the script is set executable or use %bbash %s%b instead \
+of %bsh %s%b.\n\n" 1>&2 "${yellow_text}" "${end_colour_text}" "${yellow_text}" \
+"$0" "${end_colour_text}" "${green_text}" "$0" "${end_colour_text}" \
+"${green_text}" "$0" "${end_colour_text}" "${yellow_text}" "$0" \
+"${end_colour_text}"
+    exit 1
+fi
+
+# Check we have flac installed and available for use.
+if ! [ -x "$(command -v oggdec)" ]
+then
+    printf "\n%b=== CRITICAL ===%b\n" "${red_text}" "${end_colour_text}"
+    printf "\n'oggdec' could not be found.\n"
+    printf "\nPlease check you have 'oggdec' installed on your system.\n"
+    printf "\nUnable to continue, exiting program.\n\n"
+    exit
+fi
+
+# Notify user that conversion task is starting.
+printf "\n%b>>> Conversion task is starting. <<<%b\n" "${yellow_text}" \
+"${end_colour_text}"
+
+# Convert audio file Ogg Vorbis (ogg) to Waveform Audio File Format (wav).
+for in_filename in *.[oO][gG][gG]
+do
+    oggdec -d "$in_filename"
+done
+
+# Notify user that the task has completed.
+printf "\n%b>>> Conversion task is complete. <<<%b\n" "${green_text}" \
+"${end_colour_text}"
+
+# Shameless plug to support poor Free/Open Source developers like myself who
+# make content available to people for free with no strings attached.
+printf "\nPlease consider supporting Free/Open Source developers like myself \
+who provide content for free. Voluntary donations can be made at \
+%bhttps://www.buymeacoffee.com/kathenasorg%b\n\n" "${green_text}" \
+"${end_colour_text}"
